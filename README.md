4684b237b08d9b482b0318874d292ee7ff138c1f
Sergio Romera <sergio.romera@enterprisedb.com>
2025-10-29 15:02:27 +0100

    Azure blob storage compatibility + DR

--- README.md
+++ README.md
@@ -1,7 +1,47 @@
-# CNPG demo for Macos and Linux
+# CNPG demo
 
-Tested with the CNPG v 1.27.1 on MacOS (arm) and Linux (debian)
+CNPG version tested is 1.27.1.
 
-Docker, k3d and kubectl should be preinstalled.
+# Demo on MacOS and Linux prerequisites
+If you want to test on MacOS (arm) or Linux (debian), these are the prerequisites:
+- Docker
+- k3d
+- kubectl
 
-The deck and workshop hands-on documentation will be adapted and uploaded to the repository
+# Demo on Azure prerequisites
+If you want to test in Azure:
+- Azure Kubernetes Service (AKS)
+- az command line
+
+# Azure variables configuration
+```
+export CONTAINER_NAME="<your-value>"
+export RESOURCE_GROUP="<your-value>"
+export AZURE_CONNECTION_STRING="<your-value>"
+export AZURE_STORAGE_ACCOUNT="<your-value>"
+export AZURE_STORAGE_KEY="<your-value>"
+export AZURE_STORAGE_SAS_TOKEN="<your-value>"
+export s3_bucket="https://<AZURE_STORAEG_ACCOUNT>.blob.core.windows.net/<CONTAINER_NAME>"
+```
+
+# Demo on AWS prerequisites
+- Elastic Kubernetes Sercice (EKS)
+- aws command line
+
+## AWS variables configuration
+```
+export AWS_ACCESS_KEY_ID="<your-value>"
+export AWS_SECRET_ACCESS_KEY="<your-value>"
+export AWS_SESSION_TOKEN="<your-value>"
+```
+
+# Setup your object storage environment
+Run this command and select one of these object storages:
+```
+./set_env.sh
+Select the environment type:
+1) minio (local Minio platform)
+2) azure
+3) aws
+Enter choice [1-3]: 
+```
--- env.sh
+++ env.sh
@@ -33,7 +33,6 @@ if [[ "$k8s_context_namespace" != "$namespace" ]]; then
   exit 1
 fi
 
-#print_info "================================================================================================\n"
 print_info "----------------------------------------------------------------------------------------------\n"
 print_info "⎈ Kubernetes environment ⎈\n"
 print_info "----------------------------------------------------------------------------------------------\n"
@@ -62,10 +61,27 @@ print_info "\n"
 print_info "----------------------------------------------------------------------------------------------\n"
 print_info "Object Storage\n"
 print_info "----------------------------------------------------------------------------------------------\n"
-print_info "Object Storage type        : ${yellow}${object_storage_type}\n"
-print_info "Object Storage bucket      : ${yellow}${object_storage_bucket}\n"
-print_info "Destination path           : ${yellow}${s3_destination_path}\n"
-print_info "Endpoint                   : ${yellow}${s3_endpoint_url}\n"
+
+if [ "$object_storage_type" = "minio" ]; then
+  print_info "Object Storage type        : ${yellow}${object_storage_type}\n"
+  print_info "Object Storage bucket      : ${yellow}${object_storage_bucket}\n"
+  print_info "Destination path           : ${yellow}${s3_destination_path}\n"
+  print_info "Endpoint                   : ${yellow}${s3_endpoint_url}\n"
+elif [ "$object_storage_type" = "aws" ]; then
+  print_info "Object Storage type        : ${yellow}${object_storage_type}\n"
+  print_info "Object Storage bucket      : ${yellow}${object_storage_bucket}\n"
+  print_info "Destination path           : ${yellow}${s3_destination_path}\n"
+  print_info "Endpoint                   : ${yellow}${s3_endpoint_url}\n"
+elif [ "$object_storage_type" = "azure" ]; then
+  print_info "Object Storage type        : ${yellow}${object_storage_type}\n"
+  print_info "Container name             : ${yellow}${CONTAINER_NAME}\n"
+  print_info "Resource Group             : ${yellow}${RESOURCE_GROUP}\n"
+  print_info "Azure connection string    : ${yellow}$( [ -n "${AZURE_CONNECTION_STRING}" ] && echo "***" || echo "MISSING" )${nc}\n"
+  print_info "Azure Storage Account      : ${yellow}$( [ -n "${AZURE_STORAGE_ACCOUNT}" ] && echo "***" || echo "MISSING" )${nc}\n"
+  print_info "Azure Storage Key          : ${yellow}$( [ -n "${AZURE_STORAGE_KEY}" ] && echo "***" || echo "MISSING" )${nc}\n"
+  print_info "Azure Storage Sas Token    : ${yellow}$( [ -n "${AZURE_STORAGE_SAS_TOKEN}" ] && echo "***" || echo "MISSING" )${nc}\n"
+fi
+
 print_info "\n"
 print_info "----------------------------------------------------------------------------------------------\n"
 print_info "Transparent Data Encryption (TDE)\n"
